b3357866bb780430e885afdf18b985b459e0cdaf
krish lanjewar <lanjewarkrish07@gmail.com>
2025-04-17 19:57:57 +0530

    comments

--- basicOfJava/helloJava.java
+++ basicOfJava/helloJava.java
@@ -4,6 +4,13 @@ public class helloJava{ // class name should be same as file name
         
          System.out.println("hello , Java! "); // print hello, Java! to the console
         
-         System.out.println("I am excied to learn Java");// ; is nessary to end the statement
+         System.out.print("I am excied to learn Java\n");// ; is nessary to end the statement
     }
 }
+
+// comments use for eplanation of code 
+
+/* multiple                        
+   line                          they are ignored  
+   comments                       by the compiler
+   syntax */ 
