35a80ef6da6005050c8a1853b76353956ed08fb0
krish lanjewar <lanjewarkrish07@gmail.com>
2025-04-17 19:37:18 +0530

    basics of java

---
+++ basicOfJava/helloJava.java
@@ -0,0 +1,9 @@
+public class helloJava{ // class name should be same as file name
+   
+    public static void main(String[] args){ // main method, entry point of the program
+        
+         System.out.println("hello , Java! "); // print hello, Java! to the console
+        
+         System.out.println("I am excied to learn Java");// ; is nessary to end the statement
+    }
+}
