1734558e652780e31aa5e623ddd891bba531401f
krish lanjewar <lanjewarkrish07@gmail.com>
2025-06-18 08:28:21 +0530

    improve

--- madLibs.java
+++ madLibs.java
@@ -54,39 +54,45 @@ public class madLibs {
          System.out.println("5) Romantic Comedy");
          System.out.println("6) Shinchan Chaos");
          System.out.println();
-         System.out.print("Write a STORY NAME from above( that you have chosen ):");
+         System.out.print("Write a STORY NAME from above( that you have chosen ) or WRITE ONLY NUMBER :");
          String Story = sc.nextLine();
          switch (Story.toLowerCase()) {
             case "fantasy adventure":
+            case "1":
                 System.out.println("In the land of " +place +", a "+ adjective +" knight named "+person_Name+" carried only a "+ random_Object +".\n" +
                                         "With the help of enchanted "+ plural_Noun +", they began "+verb+" across the cursed valley.\n" +
                                         "Suddenly , "+ celebrity +" appeared riding a giant "+animal+" and shouted, \""+shout+"!\"");
                 break;
             case "sci-fi space mission":
+            case "2":
             System.out.println("On planet " + place + ", the air was thick with " + adjective + " fog.\nCommander " +
                 person_Name + " adjusted their " + random_Object + ", avoiding the swarm of " + plural_Noun + ".\nWhile " +
                 verb + ", they were contacted by " + celebrity + ", who warned of a wild " + animal +
                 " approaching.\n\"" + shout + "!\" they yelled.");
                 break;
             case "school day chaos":
+            case "3":
               System.out.println("It was a regular day at " + place + " until a " + adjective + " teacher named " + person_Name +
                 " dropped a " + random_Object + " on the floor.\nThe class burst into laughter, throwing " + plural_Noun +
                 " and " + verb + " across the room.\nSuddenly, " + celebrity + " ran in, riding a " + animal +
                 ", and shouted, \"" + shout + "!\"");
                 break;
             case "party disaster":
+            case "4":
              System.out.println("The party at " + place + " started with a " + adjective + " vibe.\nHost " + person_Name +
                 " showed up late carrying a " + random_Object + " and passed around some weird " + plural_Noun +
                 ".\nPeople were " + verb + " when " + celebrity + " burst in on a dancing " + animal +
                 " yelling, \"" + shout + "!\"");
                 break;
             case "romantic comedy":
+            case "5":
             System.out.println("They first met at " + place + ", during a " + adjective + " afternoon.\n" + person_Name +
                 " was holding a " + random_Object + ", tripping over " + plural_Noun + ", and " + verb +
                 " like no one was watching.\nThen " + celebrity + " appeared, handed over a " + animal +
                 ", and whispered, \"" + shout + ".\"");
                 break;
             case "shinchan chaos":
+            case "6":
              System.out.println("Misae sent Shinchan to " + place + ", but the " + adjective + " boy named " + person_Name +
                 " had other plans.\nHe took a " + random_Object + ", gathered some " + plural_Noun + ", and started " +
                 verb + " in the street.\n" + celebrity + " saw him riding a " + animal +
